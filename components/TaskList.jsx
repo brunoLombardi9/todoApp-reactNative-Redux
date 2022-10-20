dcb1b1b4a5407774c6ec7d965860d50f32f43c4d
Bruno <juegos.malafar@gmail.com>
2022-10-20 03:44:52 -0300

    se continua

--- components/TaskList.jsx
+++ components/TaskList.jsx
@@ -1,13 +1,27 @@
-import React from "react";
-import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {Button, FlatList, Modal, Pressable, StyleSheet, Text, View} from "react-native";
 import { useDispatch } from "react-redux";
 
 const TaskList = ({ tasks }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
+  const [id, setId] = useState("");
 
-    function deleteTask(id) {
-      dispatch(taskActions.deleteTask(id));
-    }
+  function tryDelete(id) {
+    setId(id);
+    setShowModal(true);
+  }
+
+  function dontDelete() {
+    setShowModal(false);
+  }
+
+  function confirmDelete(id) {
+    dispatch(taskActions.deleteTask(id));
+    dontDelete();
+    setId("");
+  }
+
 
   return (
     <>
@@ -19,18 +33,37 @@ const TaskList = ({ tasks }) => {
           const item = itemData.item;
 
           return (
-            <View style={styles.container}>
-              <Text style={styles.task}>{item.task}</Text>
-              <Button
-                style={styles.deleteButton}
-                color="red"
-                title="X"
-                onPress={() => deleteTask(item.id)}
-              />
-            </View>
+            <Pressable
+              android_ripple={{ color: "#1a1c1e" }}
+              style={styles.pressable}
+            >
+              <View style={styles.container}>
+                <Text style={styles.task}>{item.task}</Text>
+                <Button
+                  style={styles.deleteButton}
+                  color="#b10001"
+                  title="X"
+                  onPress={() => tryDelete(item.id)}
+                />
+              </View>
+            </Pressable>
           );
         }}
       />
+
+      <Modal visible={showModal} animationType="slide">
+        <View style={styles.modalBackground}>
+          <View style={styles.modal}>
+            <Text style={styles.modalText}>
+              ¿Desea borrar la tarea seleccionada?
+            </Text>
+            <View style={styles.modalOptionsContainer}>
+              <Button title="Borrar" color="#b10001" onPress={() => confirmDelete(id)} />
+              <Button title="Volver" color="#224B0C" onPress={dontDelete} />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </>
   );
 };
@@ -40,7 +73,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#2b2a33",
     padding: 10,
     borderRadius: 5,
-    marginTop: 10,
     flexDirection: "row",
     alignItems: "center",
   },
@@ -50,6 +82,35 @@ const styles = StyleSheet.create({
   },
   deleteButton: {
     flex: 1,
+  },
+  pressable: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    padding: 2,
+    borderRadius: 10,
+  },
+  modalBackground: {
+    backgroundColor: "#2b2a33",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modal: {
+    backgroundColor: "white",
+    width: "80%",
+    borderWidth: 2,
+    padding: 20,
+    borderRadius: 8,
+  },
+  modalOptionsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 10,
+  },
+  modalText: {
+    fontSize: 20,
+    textAlign: "center",
   },
 });
 
